0479d2420855a28c92b6050401bc9c05a0ae5044
Pedro Dias <psdias.pedro@gmail.com>
2022-04-06 12:08:49 -0300

    refactoring

--- src/render.c
+++ src/render.c
@@ -6,11 +6,19 @@
 /*   By: paugusto <paugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:45:00 by paugusto          #+#    #+#             */
-/*   Updated: 2022/04/06 12:01:43 by paugusto         ###   ########.fr       */
+/*   Updated: 2022/04/06 12:08:05 by paugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
+
+void	update_frame(t_game *game)
+{
+	draw_background(game);
+	player_rotate(game);
+	player_walk_up_down(game);
+	player_walk_left_right(game);
+}
 
 int	render(t_game *game)
 {
