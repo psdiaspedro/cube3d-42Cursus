c6a6dbcce72e93b5ef77eabe713651b3f51d9de5
Pedro Dias <psdias.pedro@gmail.com>
2022-05-20 09:37:08 -0300

    cleaning some functions

--- src/texture.c
+++ src/texture.c
@@ -3,28 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   texture.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dalves-s <dalves-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: paugusto <paugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 21:08:53 by paugusto          #+#    #+#             */
-/*   Updated: 2022/04/28 10:43:00 by dalves-s         ###   ########.fr       */
+/*   Updated: 2022/05/20 09:35:38 by paugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
-
-//texel = texture pixel
-
-/*
-	- para a versão texturisada o core é o mesmo com diferença
-	no final com um codigo a mais.
-
-	- um loop no eixo y é necessario para passar por cada pixel para determinar
-	qual texel da textura deve ser usado
-
-	- cada pixel tem que ser desenhado separado
-
-
-*/
 
 void	solve_mirror(t_vec *ray_dir, t_rays *rays, t_draw *draw)
 {
@@ -34,7 +20,6 @@ void	solve_mirror(t_vec *ray_dir, t_rays *rays, t_draw *draw)
 		draw->tex_x = TEX_WIDTH - draw->tex_x - 1;
 }
 
-//pega a cordenada x da textura, que permanece a mesma
 void	get_texture_x(t_game *game, t_vec *ray_dir, t_draw *draw, t_rays *rays)
 {
 	double	wall_x;
@@ -76,34 +61,6 @@ t_data	*get_texture(t_game *game, t_vec *ray_dir, t_rays *rays)
 			return (&game->img.no);
 	}
 	return ((t_data *)0);
-}
-
-/*
-	agora que ja temos o x, precisamos de um loop no y
-	(pois o x permanece o mesmo) tex_y
-*/
-void	get_texture_y(t_game *game, t_vec *ray_dir, t_draw *draw, t_rays *rays)
-{
-	int	sl;
-	int	color;
-
-	draw->step = 1 * TEX_HEIGHT / draw->wall_line_height;
-	draw->tex_pos = (draw->start_line - HEIGHT / 2 \
-	+ draw->wall_line_height / 2) * draw->step;
-	draw->data = get_texture(game, ray_dir, rays);
-	sl = draw->start_line;
-	while (sl < draw->end_line)
-	{
-		draw->tex_y = (int)draw->tex_pos & (TEX_HEIGHT - 1);
-		draw->tex_pos += draw->step;
-		color = get_pixel(draw->data, \
-		(t_vec){.x = draw->tex_x, .y = draw->tex_y});
-		if (rays->hit_side == 1)
-			color = 65280;
-		my_mlx_pixel_put(&game->canvas, \
-		(t_vec){.x = rays->rays, .y = sl}, color);
-		sl++;
-	}
 }
 
 void	get_wall_size(t_game *game, t_vec *ray_dir, t_rays *rays, int pixel)
